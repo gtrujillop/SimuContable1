0c757535f45c267c9f8ef8ef87f36fb76e2dd5f4
gtrujillop <gtrujillo@tecnocomfenalco.edu.co>
2014-03-19 22:23:27 -0500

    Commit Inicial SimuContable

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SimuladoContable1::Application.config.secret_key_base = 'a4702f5a123f8912b89127f6c8f3d3dcb0e8df73f414c214f0b1a126698f2d7d8bf6b9e84c48dbceb84f15072e81fb05c2782b450b1e7bd4753d609d55f6f10d'
